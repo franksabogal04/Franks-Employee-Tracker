b043debe9c8e7265a33f498c70201d451037aee1
franksabogal04 <franksabogal04@gmail.com>
2023-06-08 23:49:14 -0400

    created the server.js to run app

---
+++ server.js
@@ -0,0 +1,58 @@
+// Importing the necessary packages and modules for a working code
+const inquirer = require('inquirer');
+const { getAllDepartments } = require('./db/queries');
+const ConsoleT = require('console.table');
+
+
+// Function for departments to be displayed
+function DepartmentsDrop() {
+    getAllDepartments()
+        .then((departments) => {
+            console.log('Departments');
+
+            // use variable for console.table
+            ConsoleT(departments);
+            CentralDispaly();
+        })
+        .catch((error) => {
+            console.log('this acction has created an error please try again', error);
+            CentralDispaly();
+        });
+}
+
+
+// Function to show the beginning of the app
+function CentralDispaly() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'whats the next step?',
+                choices: [
+                    'Retrieve all departments',
+                    'Quit',
+                ],
+            },
+        ])
+        .then((answers) => {
+            switch (answers.action) {
+                case 'Retrieve all departments':
+                    DepartmentsDrop();
+                    break;
+
+                case 'Quit':
+                    console.log('see ya!');
+                    process.exit();
+
+                default:
+                    console.log('this option is not availble. please try again!');
+                    CentralDispaly();
+            }
+        });
+}
+
+// the centraldisplay will be called in order to start the app
+CentralDispaly();
+
+
